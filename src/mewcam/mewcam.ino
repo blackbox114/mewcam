434510896e6da49cac124ac154c0270f6420738f
blackbox114 <chen.junfei@nexuslink.cn>
2020-07-21 12:12:17 +0800

    尝试巴法云的例程

--- src/mewcam/mewcam.ino
+++ src/mewcam/mewcam.ino
@@ -1,212 +1,73 @@
-//原作者信息：
-/*********
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp32-cam-video-streaming-web-server-camera-home-assistant/
-  
-  IMPORTANT!!! 
-   - Select Board "AI Thinker ESP32-CAM"
-   - GPIO 0 must be connected to GND to upload a sketch
-   - After connecting GPIO 0 to GND, press the ESP32-CAM on-board RESET button to put your board in flashing mode
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
+/*巴法云的例程
+ * 2020-07-07
+ * QQ交流群：824273231
+ * 微信：19092550573
+ * 官网https://bemfa.com  
+ *
+ *此版本需要json库，再arduino IDE 选项栏，点击“工具”-->"管理库"-->搜索arduinojson 第一个库就是，点击安装即可
+ *
+分辨率默认配置：config.frame_size = FRAMESIZE_UXGA;
+其他配置：
+FRAMESIZE_UXGA （1600 x 1200）
+FRAMESIZE_QVGA （320 x 240）
+FRAMESIZE_CIF （352 x 288）
+FRAMESIZE_VGA （640 x 480）
+FRAMESIZE_SVGA （800 x 600）
+FRAMESIZE_XGA （1024 x 768）
+FRAMESIZE_SXGA （1280 x 1024）
 
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*********/
-//测试效果：卡顿和延迟出现，发热严重，本地局域网内传输也存在严重卡顿，无法满足视频流传输要求。可能会尝试其他技术
+config.jpeg_quality = 10;（10-63）越小照片质量最好
+数字越小表示质量越高，但是，如果图像质量的数字过低，尤其是在高分辨率时，可能会导致ESP32-CAM崩溃
 
+*支持发布订阅模式，当图片上传时，订阅端会自动获取图片url地址，可做图片识别，人脸识别，图像分析
+*/
+
+#include <HTTPClient.h>
 #include "esp_camera.h"
 #include <WiFi.h>
-#include "esp_timer.h"
-#include "img_converters.h"
-#include "Arduino.h"
-#include "fb_gfx.h"
-#include "soc/soc.h" //disable brownout problems
-#include "soc/rtc_cntl_reg.h"  //disable brownout problems
-#include "esp_http_server.h"
+#include <ArduinoJson.h>
 
-//Replace with your network credentials
-const char* ssid = "LBm";
-const char* password = "11111111";
+/*********************需要修改的地方**********************/
+const char* ssid = "newhtc";           //WIFI名称
+const char* password = "qq123456";     //WIFI密码
+int capture_interval = 20*1000;        // 默认20秒上传一次，可更改（本项目是自动上传，如需条件触发上传，在需要上传的时候，调用take_send_photo()即可）
+const char*  post_url = "http://images.bemfa.com/upload/v1/upimages.php"; // 默认上传地址
+const char*  uid = "4d9ec352e0376f2110a0c601a2857225";    //用户私钥，巴法云控制台获取
+const char*  topic = "mypicture";     //主题名字，可在控制台新建
+/********************************************************/
 
-#define PART_BOUNDARY "123456789000000000000987654321"
 
-// This project was tested with the AI Thinker Model, M5STACK PSRAM Model and M5STACK WITHOUT PSRAM
-#define CAMERA_MODEL_AI_THINKER
-//#define CAMERA_MODEL_M5STACK_PSRAM
-//#define CAMERA_MODEL_M5STACK_WITHOUT_PSRAM
+bool internet_connected = false;
+long current_millis;
+long last_capture_millis = 0;
 
-// Not tested with this model
-//#define CAMERA_MODEL_WROVER_KIT
+// CAMERA_MODEL_AI_THINKER
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
 
-#if defined(CAMERA_MODEL_WROVER_KIT)
-  #define PWDN_GPIO_NUM    -1
-  #define RESET_GPIO_NUM   -1
-  #define XCLK_GPIO_NUM    21
-  #define SIOD_GPIO_NUM    26
-  #define SIOC_GPIO_NUM    27
-  
-  #define Y9_GPIO_NUM      35
-  #define Y8_GPIO_NUM      34
-  #define Y7_GPIO_NUM      39
-  #define Y6_GPIO_NUM      36
-  #define Y5_GPIO_NUM      19
-  #define Y4_GPIO_NUM      18
-  #define Y3_GPIO_NUM       5
-  #define Y2_GPIO_NUM       4
-  #define VSYNC_GPIO_NUM   25
-  #define HREF_GPIO_NUM    23
-  #define PCLK_GPIO_NUM    22
-
-#elif defined(CAMERA_MODEL_M5STACK_PSRAM)
-  #define PWDN_GPIO_NUM     -1
-  #define RESET_GPIO_NUM    15
-  #define XCLK_GPIO_NUM     27
-  #define SIOD_GPIO_NUM     25
-  #define SIOC_GPIO_NUM     23
-  
-  #define Y9_GPIO_NUM       19
-  #define Y8_GPIO_NUM       36
-  #define Y7_GPIO_NUM       18
-  #define Y6_GPIO_NUM       39
-  #define Y5_GPIO_NUM        5
-  #define Y4_GPIO_NUM       34
-  #define Y3_GPIO_NUM       35
-  #define Y2_GPIO_NUM       32
-  #define VSYNC_GPIO_NUM    22
-  #define HREF_GPIO_NUM     26
-  #define PCLK_GPIO_NUM     21
-
-#elif defined(CAMERA_MODEL_M5STACK_WITHOUT_PSRAM)
-  #define PWDN_GPIO_NUM     -1
-  #define RESET_GPIO_NUM    15
-  #define XCLK_GPIO_NUM     27
-  #define SIOD_GPIO_NUM     25
-  #define SIOC_GPIO_NUM     23
-  
-  #define Y9_GPIO_NUM       19
-  #define Y8_GPIO_NUM       36
-  #define Y7_GPIO_NUM       18
-  #define Y6_GPIO_NUM       39
-  #define Y5_GPIO_NUM        5
-  #define Y4_GPIO_NUM       34
-  #define Y3_GPIO_NUM       35
-  #define Y2_GPIO_NUM       17
-  #define VSYNC_GPIO_NUM    22
-  #define HREF_GPIO_NUM     26
-  #define PCLK_GPIO_NUM     21
-
-#elif defined(CAMERA_MODEL_AI_THINKER)
-  #define PWDN_GPIO_NUM     32
-  #define RESET_GPIO_NUM    -1
-  #define XCLK_GPIO_NUM      0
-  #define SIOD_GPIO_NUM     26
-  #define SIOC_GPIO_NUM     27
-  
-  #define Y9_GPIO_NUM       35
-  #define Y8_GPIO_NUM       34
-  #define Y7_GPIO_NUM       39
-  #define Y6_GPIO_NUM       36
-  #define Y5_GPIO_NUM       21
-  #define Y4_GPIO_NUM       19
-  #define Y3_GPIO_NUM       18
-  #define Y2_GPIO_NUM        5
-  #define VSYNC_GPIO_NUM    25
-  #define HREF_GPIO_NUM     23
-  #define PCLK_GPIO_NUM     22
-#else
-  #error "Camera model not selected"
-#endif
-
-static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
-static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
-static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
-
-httpd_handle_t stream_httpd = NULL;
-
-static esp_err_t stream_handler(httpd_req_t *req){
-  camera_fb_t * fb = NULL;
-  esp_err_t res = ESP_OK;
-  size_t _jpg_buf_len = 0;
-  uint8_t * _jpg_buf = NULL;
-  char * part_buf[64];
-
-  res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
-  if(res != ESP_OK){
-    return res;
-  }
-
-  while(true){
-    fb = esp_camera_fb_get();
-    if (!fb) {
-      Serial.println("Camera capture failed");
-      res = ESP_FAIL;
-    } else {
-      if(fb->width > 400){
-        if(fb->format != PIXFORMAT_JPEG){
-          bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
-          esp_camera_fb_return(fb);
-          fb = NULL;
-          if(!jpeg_converted){
-            Serial.println("JPEG compression failed");
-            res = ESP_FAIL;
-          }
-        } else {
-          _jpg_buf_len = fb->len;
-          _jpg_buf = fb->buf;
-        }
-      }
-    }
-    if(res == ESP_OK){
-      size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
-      res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
-    }
-    if(res == ESP_OK){
-      res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
-    }
-    if(res == ESP_OK){
-      res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
-    }
-    if(fb){
-      esp_camera_fb_return(fb);
-      fb = NULL;
-      _jpg_buf = NULL;
-    } else if(_jpg_buf){
-      free(_jpg_buf);
-      _jpg_buf = NULL;
-    }
-    if(res != ESP_OK){
-      break;
-    }
-    //Serial.printf("MJPG: %uB\n",(uint32_t)(_jpg_buf_len));
-  }
-  return res;
-}
-
-void startCameraServer(){
-  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
-  config.server_port = 80;
-
-  httpd_uri_t index_uri = {
-    .uri       = "/",
-    .method    = HTTP_GET,
-    .handler   = stream_handler,
-    .user_ctx  = NULL
-  };
-  
-  //Serial.printf("Starting web server on port: '%d'\n", config.server_port);
-  if (httpd_start(&stream_httpd, &config) == ESP_OK) {
-    httpd_register_uri_handler(stream_httpd, &index_uri);
-  }
-}
-
-void setup() {
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
- 
+void setup()
+{
   Serial.begin(115200);
-  Serial.setDebugOutput(false);
-  
+
+  if (init_wifi()) { // Connected to WiFi
+    internet_connected = true;
+    Serial.println("Internet connected");
+  }
+
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -227,9 +88,9 @@ void setup() {
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG; 
-  
-  if(psramFound()){
+  config.pixel_format = PIXFORMAT_JPEG;
+  //init with high specs to pre-allocate larger buffers
+  if (psramFound()) {
     config.frame_size = FRAMESIZE_UXGA;
     config.jpeg_quality = 10;
     config.fb_count = 2;
@@ -238,29 +99,102 @@ void setup() {
     config.jpeg_quality = 12;
     config.fb_count = 1;
   }
-  
-  // Camera init
+
+  // camera init
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
     Serial.printf("Camera init failed with error 0x%x", err);
     return;
   }
-  // Wi-Fi connection
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  
-  Serial.print("Camera Stream Ready! Go to: http://");
-  Serial.print(WiFi.localIP());
-  
-  // Start streaming web server
-  startCameraServer();
 }
 
-void loop() {
-  delay(1);
+
+/********初始化WIFI*********/
+bool init_wifi()
+{
+  int connAttempts = 0;
+  Serial.println("\r\nConnecting to: " + String(ssid));
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED ) {
+    delay(500);
+    Serial.print(".");
+    if (connAttempts > 10) return false;
+    connAttempts++;
+  }
+  return true;
+}
+
+
+
+/********推送图片*********/
+static esp_err_t take_send_photo()
+{
+    //初始化相机并拍照
+    Serial.println("Taking picture...");
+    camera_fb_t * fb = NULL;
+    fb = esp_camera_fb_get();
+    if (!fb) {
+      Serial.println("Camera capture failed");
+      return ESP_FAIL;
+    }
+  
+    HTTPClient http;
+    //设置请求url
+    http.begin(post_url);
+    
+    //设置请求头部信息
+    http.addHeader("Content-Type", "image/jpg");
+    http.addHeader("Authorization", uid);
+    http.addHeader("Authtopic", topic);
+    
+    //发起请求，并获取状态码
+    int httpResponseCode = http.POST((uint8_t *)fb->buf, fb->len);
+    
+    if(httpResponseCode==200){
+        //获取post请求后的服务器响应信息，json格式
+        String response = http.getString();  //Get the response to the request
+        Serial.print("Response Msg:");
+        Serial.println(response);           // 打印服务器返回的信息
+        
+
+        //json数据解析
+        StaticJsonDocument<200> doc;
+        DeserializationError error = deserializeJson(doc, response);
+        if (error) {
+          Serial.print(F("deserializeJson() failed: "));
+          Serial.println(error.c_str());
+        }
+        const char* url = doc["url"];
+        Serial.print("Get URL:");
+        Serial.println(url);//打印获取的URL
+    
+        
+    }else{
+        //错误请求
+        Serial.print("Error on sending POST: ");
+        Serial.println(httpResponseCode);
+    }
+   
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+  
+    //清空数据
+    esp_camera_fb_return(fb);  
+    //回收下次再用
+    http.end();
+  
+}
+
+
+
+void loop()
+{
+  // TODO check Wifi and reconnect if needed
+
+  //定时发送
+  current_millis = millis();
+  if (current_millis - last_capture_millis > capture_interval) { // 当前时间减去上次时间大于20S就执行拍照上传函数
+    last_capture_millis = millis();
+    take_send_photo(); //拍照上传函数，在需要的地方调用即可，这里是定时拍照
+  }
 }
